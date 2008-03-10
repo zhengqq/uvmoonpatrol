8f6fe91f3c0b486269853678f1bae8f082075ffe
cthulhu32 <cthulhu32@591a4e72-d347-0410-ad9e-b354b1ae3366>
2008-03-10 20:14:29 +0000

    Don't want to lose my main.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -5,133 +5,20 @@
 #include <SDL/SDL.h>
 #include <cstdlib>
 #include <ctime>
+#include "base.h"
+#include "gfx.h"
+#include "car.h"
 
-#define BOOL    int
-#define FALSE   0
-#define TRUE    1
-#define COLORKEY 0,255,0 // transparent color
-#define KEY_R 0
-#define KEY_G 255
-#define KEY_B 0
-// Physics iterations per second
-#define PHYSICSFPS 30
-
+int         gLastFrame;
 int         gLastTick;
-Uint8*		keys;			// Array Used For The Keyboard Routine
 BOOL		active=TRUE;		// Window Active Flag Set To TRUE By Default
-BOOL		fullscreen=TRUE;	// Fullscreen Flag Set To Fullscreen Mode By Default
+BOOL		fullscreen=FALSE;	// Fullscreen Flag Set To Fullscreen Mode By Default
+BOOL        gKeyLeft,gKeyRight,gKeyUp,gKeyDown;
 
-int gTextureCount = 12; // how many textures we have
-char * gTextureList[12] = {"data\\bus1_1.bmp","data\\bus1_2.bmp","data\\car.bmp",
-                    "data\\flyingman.bmp","data\\running1_1.bmp","data\\running1_2.bmp",
-                    "data\\wheel1_1.bmp","data\\wheel1_2.bmp","data\\wheel2_1.bmp",
-                    "data\\wheel2_2.bmp","data\\wheel3_1.bmp","data\\wheel3_2.bmp"};
-
-struct Sprite
-{
-	GLuint texture;
-	char * name;
-	int width;
-	int height;
-	int x;
-	int y;
-};
-
-Sprite		sprites[12];
-
-SDL_Surface *LoadBMP2RGBA(char *filename)
-{
-  Uint8 *rowhi, *rowlo;
-  Uint8 *tmpbuf, tmpch;
-  SDL_Surface *image;
-  int i, j;
-  image = SDL_LoadBMP(filename);
-  if ( image == NULL ) {
-    fprintf(stderr, "Unable to load %s: %s\n", filename, SDL_GetError());
-    return(NULL);
-  }
-  /* Copy our RGB into an ABGR buffer - C32 */
-  // Allocated all 4 pixels for our ABGR
-    unsigned char * newPixels = (unsigned char *)malloc((image->w*image->h*4));
-    unsigned char * newPixelPtr = newPixels;
-    unsigned char * pixelPtr = (unsigned char *)image->pixels;
-    for(int i = 0; i < (image->w*image->h);i++)
-    {
-        unsigned char * R = pixelPtr;
-        unsigned char * G = R + sizeof(unsigned char);
-        unsigned char * B = G + sizeof(unsigned char);
-        // Lets start dumping them into our new memory!
-        * newPixelPtr++ = *B;
-        * newPixelPtr++ = *G;
-        * newPixelPtr++ = *R;
-        if ( *B == KEY_B && *G == KEY_G && *R == KEY_R )
-        {
-            * newPixelPtr++ = 0;
-        }
-        else
-        {
-            * newPixelPtr++ = 255;
-        }
-        pixelPtr += sizeof(unsigned char)*3;
-    }
-    free(image->pixels);        // free the pixels used in the original LoadBMP
-    image->pixels = newPixels;  // set our current image pixels to our new pixels
-    image->pitch = (image->pitch/3)*4; // adjust our pitch for 4 pixels instead of 3
-    // Now flip our surface for OpenGL specifics!
-    tmpbuf = (Uint8 *)malloc(image->pitch);
-    if ( tmpbuf == NULL ) {
-        fprintf(stderr, "Out of memory\n");
-        return(NULL);
-    }
-    rowhi = (Uint8 *)image->pixels; // highest portion of the image
-    rowlo = rowhi + (image->h * image->pitch) - image->pitch; // lowest portion
-    for ( i=0; i<image->h/2; ++i ) {
-        memcpy(tmpbuf, rowhi, image->pitch);
-        memcpy(rowhi, rowlo, image->pitch);
-        memcpy(rowlo, tmpbuf, image->pitch);
-        rowhi += image->pitch;
-        rowlo -= image->pitch;
-    }
-  return(image);
-}
-
-int LoadGLTextures() 		// Load Bitmaps And Convert To Textures
-{
-    int noError=TRUE;		// Status Indicator
-    SDL_Surface *TextureImage[gTextureCount];	// Create Storage Space For The Textures
-    memset(TextureImage,0,sizeof(void *)*gTextureCount);	// Set The Pointer To NULL
-    for(int i = 0; i < gTextureCount; i++)
-    {
-        TextureImage[i]=LoadBMP2RGBA(gTextureList[i]);
-        if (TextureImage[i]==NULL)
-        {
-            printf("Error, could not load textures!\n");
-            noError=FALSE;
-            break;
-        }
-        glGenTextures(1, &sprites[i].texture);		// Create The Texture
-        glBindTexture(GL_TEXTURE_2D, sprites[i].texture);
-        gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, TextureImage[i]->w, TextureImage[i]->h, GL_RGBA, GL_UNSIGNED_BYTE, TextureImage[i]->pixels);
-        //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TextureImage[loop]->w, TextureImage[loop]->h,
-        //    0, GL_RGBA, GL_UNSIGNED_BYTE, TextureImage[loop]->pixels);
-        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-        sprites[i].width = TextureImage[i]->w;
-        sprites[i].height = TextureImage[i]->h;
-        sprites[i].x = sprites[i].y = 0; // ### DEBUG! ###
-        sprites[i].name = gTextureList[i];
-        if (TextureImage[i]) {		// If Texture Exists
-            SDL_FreeSurface(TextureImage[i]);
-        }
-    }
-    return noError;
-}
+Car*        mainCar;
 
 int InitGL(GLvoid)			// All Setup For OpenGL Goes Here
 {
-  if (!LoadGLTextures())		// If Loading The Textures Failed
-    return FALSE;
-
 	glViewport(0, 0,240, 248);
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glDisable(GL_DEPTH_TEST);
@@ -149,72 +36,42 @@ int InitGL(GLvoid)			// All Setup For OpenGL Goes Here
 	return TRUE;						// Initialization Went OK
 }
 
-void DrawSprite(Sprite & sprite, int x, int y, bool flip)
-{
-//	if(x>camera_x+160 || y>camera_y+120 || x+sprite.width<camera_x-160 || y+sprite.height<camera_y-120)
-//		return;
-	float xflip=0.0f;
-	if(flip) xflip=1.0f;
-	glBindTexture(GL_TEXTURE_2D, sprite.texture);
-	glBegin(GL_QUADS);
-	glTexCoord2f(0.0f+xflip, 0.0f);
-	glVertex3i(x, y, 0);
-	glTexCoord2f(0.0f+xflip, 1.0f);
-	glVertex3i(x, y+sprite.height, 0);
-	glTexCoord2f(1.0f-xflip, 1.0f);
-	glVertex3i(x+sprite.width, y+sprite.height, 0);
-	glTexCoord2f(1.0f-xflip, 0.0f);
-	glVertex3i(x+sprite.width, y, 0);
-	glEnd();
-}
-
 int DrawGLScene(GLvoid)				// Draw Everything
 {
-    bool sceneDrawn = FALSE;
     int tick = SDL_GetTicks();
-    if ( tick <= gLastTick)
+    if (tick <= gLastFrame && (((1000/PHYSICSFPS)-(tick-gLastTick)>0)))
     {
-        SDL_Delay(1);
+        SDL_Delay((1000 / PHYSICSFPS) - (tick-gLastTick));
         return TRUE;
     }
-    while (gLastTick < tick)
+    gLastTick = tick;
+    while (gLastFrame < tick)
     {
-        if ( !sceneDrawn )
-        {
-            // Clear Screen, Depth Buffer & Stencil Buffer
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        if (gKeyLeft) mainCar->moveLeft();
+        if (gKeyRight) mainCar->moveRight();
+        if (gKeyUp) gKeyUp=gKeyUp;
+        if (gKeyDown) gKeyDown=gKeyDown;
 
-            // Draw our sprites
-            glPushMatrix();
-            glLoadIdentity();
-            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            for(int i = 0; i < gTextureCount; i++)
-                DrawSprite(sprites[i], sprites[i].x, sprites[i].y, false);
+        // Physics updates go here!
 
-            glPopMatrix();
-            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            glFlush();
-            sceneDrawn = TRUE;
-        }
-        gLastTick += 1000 / PHYSICSFPS;
+        gLastFrame += 1000 / PHYSICSFPS;
     }
+
+    // Clear Screen, Depth Buffer & Stencil Buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Draw our sprites
+    glPushMatrix();
+    glLoadIdentity();
+    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+    mainCar->draw();
+
+    glPopMatrix();
+    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+    glFlush();
+
     return TRUE;					// Everything Went OK
-}
-
-void ProcessKeyboard()				// Process Keyboard Results
-{
-    /*
-  if (keys[SDLK_RIGHT])	yrotspeed += 0.08f;	// Right Arrow Pressed (Increase yrotspeed)
-  if (keys[SDLK_LEFT])	yrotspeed -= 0.08f;	// Left Arrow Pressed (Decrease yrotspeed)
-  if (keys[SDLK_DOWN])	xrotspeed += 0.08f;	// Down Arrow Pressed (Increase xrotspeed)
-  if (keys[SDLK_UP])	xrotspeed -= 0.08f;	// Up Arrow Pressed (Decrease xrotspeed)
-
-  if (keys[SDLK_a])	zoom +=0.05f;		// 'A' Key Pressed ... Zoom In
-  if (keys[SDLK_z])	zoom -=0.05f;		// 'Z' Key Pressed ... Zoom Out
-
-  if (keys[SDLK_PAGEUP]) height +=0.03f;	// Page Up Key Pressed Move Ball Up
-  if (keys[SDLK_PAGEDOWN]) height -=0.03f;	// Page Down Key Pressed Move Ball Down
-  */
 }
 
 GLvoid KillGLWindow(GLvoid)			// Properly Kill The Window
@@ -234,7 +91,7 @@ BOOL CreateGLWindow(char* title, int width, int height, int bits, BOOL fullscree
   int size;
 
   fullscreen=fullscreenflag;	// Set The Global Fullscreen Flag
-  flags = SDL_OPENGL;
+  flags = SDL_OPENGL | SDL_HWSURFACE;
   if ( fullscreenflag ) {
     flags |= SDL_FULLSCREEN;
   }
@@ -257,26 +114,7 @@ BOOL CreateGLWindow(char* title, int width, int height, int bits, BOOL fullscree
     SDL_ShowCursor(FALSE); // hide our cursor
 
   //ReSizeGLScene(width, height);		// Set Up Our Perspective GL Screen
-
-  if (!InitGL())			// Initialize Our Newly Created GL Window
-  {
-    KillGLWindow();			// Reset The Display
-    return FALSE;			// Return FALSE
-  }
   return TRUE;				// Success
-}
-
-void UpdatePhysics()
-{
-    // We have all the sprite names, but we are going to cheat here
-    sprites[2].x = 50;
-    sprites[2].y = 100;
-    sprites[6].x = 45;
-    sprites[6].y = 100;
-    sprites[8].x = 60;
-    sprites[8].y = 100;
-    sprites[10].x = 75;
-    sprites[10].y = 100;
 }
 
 int main(int argc, char *argv[])
@@ -285,20 +123,37 @@ int main(int argc, char *argv[])
     BOOL	done=FALSE;			// Bool Variable To Exit Loop
     gLastTick = SDL_GetTicks();
 
-  /* Initialize SDL */
-  if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-    fprintf(stderr, "Couldn't init SDL: %s\n", SDL_GetError());
-    return 1;
-  }
+    gKeyLeft=gKeyRight=gKeyUp=gKeyDown=FALSE;
 
-  // Create Our OpenGL Window
-  if (!CreateGLWindow("Ultraviolence Moon Patrol", 240, 248, 32, fullscreen))
-  {
-    SDL_Quit();
-    return 0;				// Quit If Window Was Not Created
-  }
+    /* Initialize SDL */
+    if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+        fprintf(stderr, "Couldn't init SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Create Our OpenGL Window
+    if (!CreateGLWindow("Ultraviolence Moon Patrol", SCREEN_WIDTH, SCREEN_HEIGHT, 32, fullscreen))
+    {
+        SDL_Quit();
+        return 0;				// Quit If Window Was Not Created
+    }
+
+    // Call our car here!
+    mainCar = new Car();
+
+    if (!InitGL())			// Initialize Our Newly Created GL Window
+    {
+        KillGLWindow();			// Reset The Display
+        return -1;			// Return Error
+    }
 
     while(!done) {			// Loop That Runs While done=FALSE
+
+            // Draw The Scene.  Watch For ESC Key And Quit Messages From DrawGLScene()
+            if ((active && !DrawGLScene()))// Active? Was There A Quit Received?
+                done=TRUE;			// ESC or DrawGLScene Signalled A Quit
+            else				// Not Time To Quit, Update Screen
+                SDL_GL_SwapBuffers();		// Swap Buffers (Double Buffering)
 
             SDL_Event event;
             while ( SDL_PollEvent(&event) ) {
@@ -309,21 +164,57 @@ int main(int argc, char *argv[])
                 default:
                     break;
                 }
+
+                if (event.type == SDL_KEYDOWN)
+                {
+                    switch(event.key.keysym.sym){
+                        case SDLK_LEFT:
+                            gKeyLeft=TRUE;
+                        break;
+                        case SDLK_RIGHT:
+                            gKeyRight=TRUE;
+                        break;
+                        case SDLK_UP:
+                            gKeyUp=TRUE;
+                        break;
+                        case SDLK_DOWN:
+                            gKeyDown=TRUE;
+                        break;
+                        case SDLK_ESCAPE:
+                            done=TRUE;
+                        break;
+                        default:
+
+                        break;
+                    }
+                }
+                if (event.type == SDL_KEYUP)
+                {
+                    switch(event.key.keysym.sym){
+                        case SDLK_LEFT:
+                            gKeyLeft=FALSE;
+                        break;
+                        case SDLK_RIGHT:
+                            gKeyRight=FALSE;
+                        break;
+                        case SDLK_UP:
+                            gKeyUp=FALSE;
+                        break;
+                        case SDLK_DOWN:
+                            gKeyDown=FALSE;
+                        break;
+                        default:
+                        break;
+                    }
+                }
             }
-            keys = SDL_GetKeyState(NULL);
-
-            UpdatePhysics(); // Temp Function
-
-            // Draw The Scene.  Watch For ESC Key And Quit Messages From DrawGLScene()
-            if ((active && !DrawGLScene()) || keys[SDLK_ESCAPE])// Active? Was There A Quit Received?
-                done=TRUE;			// ESC or DrawGLScene Signalled A Quit
-            else				// Not Time To Quit, Update Screen
-                SDL_GL_SwapBuffers();		// Swap Buffers (Double Buffering)
-            ProcessKeyboard();			// Processed Keyboard Presses
     }
 
     // Shutdown
     KillGLWindow();			// Kill The Window
     SDL_Quit();
+
+    delete mainCar;
+
     return 0;				// Exit The Program
 }

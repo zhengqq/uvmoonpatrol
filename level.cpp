901e70bebffe90bec666145c8bac5300382577f7
cthulhu32 <cthulhu32@591a4e72-d347-0410-ad9e-b354b1ae3366>
2008-03-18 22:54:40 +0000

    More fixes (^2 textures is a big deal.)

--- level.cpp
+++ level.cpp
@@ -18,6 +18,13 @@ Level::Level()
     if(!generateSprite("data\\tile4.bmp",&tiles[3])){
         printf("Could not load tile4.bmp\n");
     }
+    if(!generateSprite("data\\spacebg1.bmp",&spaceBG)) printf("Could not load spacebg1.bmp\n");
+    if(!generateSprite("data\\farbg1.bmp",&farBG)) printf("Could not load farbg1.bmp\n");
+    if(!generateSprite("data\\closebg1.bmp",&closeBG)) printf("Could not load closebg1.bmp\n");
+
+    spaceX=0;
+    farX=0;
+    closeX=0;
 }
 
 Level::~Level()
@@ -35,6 +42,24 @@ void Level::update(int scrollX)
 
 void Level::draw()
 {
+    spaceX++;
+    if ( spaceX >= 15360) // 480 * 32
+    {
+        spaceX = 0;
+    }
+    DrawSprite(spaceBG,-int(spaceX/32), 0, FALSE);
+    farX++;
+    if ( farX >= 3840 ) // 480 * 8
+    {
+        farX = 0;
+    }
+    DrawSprite(farBG,-int(farX/8), 0, FALSE);
+    closeX++;
+    if ( closeX >= 1920 )
+    {
+        closeX = 0;
+    }
+    DrawSprite(closeBG,-int(closeX/2), 0, FALSE);
     for(int i = 0; i < 164; i++){
         DrawSprite(tiles[debugLevel[i]], i*32 - levelX, 190, FALSE);
     }
